8a25824eb69f7caedf1907bc1ae1e1513d37db18
ildizsigrai <ildi.zsigrai@gmail.com>
2024-07-04 14:35:33 +0200

    calculate the answer

--- project3.py
+++ project3.py
@@ -10,9 +10,10 @@ def generate_problem():
     operator = random.choice(OPERATORS)
 
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
-    return expr
+    answer = eval(expr)
+    return expr, answer
 
-generate_problem()
+expr, answer = generate_problem()
+print(expr, "=",  answer)
 
 
